174c02045fa59a47eff9b3bb601fd7d715429cfe
Reshma Abraham <reshmaabraham@DTAs-MacBook-Pro.local>
2019-08-06 15:03:19 +1000

    fixed slack pt3

--- subscribers/slack/worker.tests/UserMessageProcessorTest.cs
+++ subscribers/slack/worker.tests/UserMessageProcessorTest.cs
@@ -50,6 +50,7 @@ Domain: b.com";
 
             slackService.Verify((ss) => ss.SendSlackMessage(user_slack_url, message), Times.Exactly(1));
             Assert.True(result, "created should return true");
+            
         }
     }
 }
