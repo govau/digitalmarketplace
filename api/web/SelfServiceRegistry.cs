f0ad4fa96cdeda40a64b1a802cd793f0882826be
Sam <29763502+malmas18@users.noreply.github.com>
2020-05-07 10:46:34 +1000

    simplify lamar registration

--- api/web/SelfServiceRegistry.cs
+++ api/web/SelfServiceRegistry.cs
@@ -1,25 +1,10 @@
 using Lamar;
-using Dta.Marketplace.Api.Business;
-using Dta.Marketplace.Api.Services;
-using Dta.Marketplace.Api.Services.Impl;
 
 namespace Dta.Marketplace.Api.Web {
     public class SelfServiceRegistry: ServiceRegistry {
         public SelfServiceRegistry() {
             Scan(x => {
-                x.Assembly(typeof(BaseBusiness).Assembly);
-                x.WithDefaultConventions();
-            });
-            Scan(x => {
-                x.Assembly(typeof(IBusiness).Assembly);
-                x.WithDefaultConventions();
-            });
-            Scan(x => {
-                x.Assembly(typeof(BaseService).Assembly);
-                x.WithDefaultConventions();
-            });
-            Scan(x => {
-                x.Assembly(typeof(IService).Assembly);
+                x.AssembliesFromApplicationBaseDirectory(af => af.GetName().Name.StartsWith("Dta.Marketplace.Api"));
                 x.WithDefaultConventions();
             });
         }
