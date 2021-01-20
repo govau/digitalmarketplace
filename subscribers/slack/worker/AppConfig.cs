f6236762ca444d7035f3618d40f218564b7e1532
Reshma-M-A <31645577+Reshma-M-A@users.noreply.github.com>
2021-01-20 11:03:50 +1100

    removing blank space in app config

--- subscribers/slack/worker/AppConfig.cs
+++ subscribers/slack/worker/AppConfig.cs
@@ -13,7 +13,6 @@ namespace Dta.Marketplace.Subscribers.Slack.Worker {
         public string BuyerSlackUrl { get; set; }
         public string UserSlackUrl { get; set; }
         public string MailchimpSlackUrl { get; set; }
-
         public int WorkIntervalInSeconds { get; set; } = 60;
         public string SentryDsn { get; set; }
     }
