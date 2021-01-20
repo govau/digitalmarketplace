b031fcecca92fb1aecc66c1deca8625d5f51e8e9
Reshma-M-A <reshmamabraham@gmail.com>
2021-01-20 12:05:01 +1100

    removing id in selectCheck

--- ui-automated-tests/src/flows/brief/training.ts
+++ ui-automated-tests/src/flows/brief/training.ts
@@ -112,7 +112,7 @@ const fillClosingDate = async () => {
 };
 
 const fillPublishBrief = async () => {
-  await utils.selectCheck("cb-declaration", "id");
+  await utils.selectCheck("cb-declaration");
   await utils.clickButton("Publish");
   await utils.matchText("h1", "Your opportunity is now live, and the invited sellers have been notified.");
 };
