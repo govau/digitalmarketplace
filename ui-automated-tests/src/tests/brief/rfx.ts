078d037ae87e49bb42847af91316d99c08070182
Reshma-M-A <reshmamabraham@gmail.com>
2021-04-01 11:01:07 +1100

    update mocha

--- ui-automated-tests/src/tests/brief/rfx.ts
+++ ui-automated-tests/src/tests/brief/rfx.ts
@@ -3,7 +3,7 @@ import startBrief from "../../flows/dashboard/buyer";
 import { buyerLogin } from "../../flows/login/actions";
 
 describe("should be able to create RFXs opportunity", () => {
-  it("should be able to create RFXs opportunity", async () => {
+  it.only("should be able to create RFXs opportunity", async () => {
     const now = Date.now();
     await buyerLogin();
     await startBrief();
