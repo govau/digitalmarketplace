7cc2d28cc9a32edc454c2585fcfb1fdfb1303d60
Reshma-M-A <reshmamabraham@gmail.com>
2021-05-04 22:31:40 -0700

    fixed up login tests to reflect the new login screen

--- ui-automated-tests/src/tests/login/buyer.ts
+++ ui-automated-tests/src/tests/login/buyer.ts
@@ -3,12 +3,13 @@ import * as util from "../../utils";
 
 describe("should fail sign in", function() {
   const testCases = [
-    { args: ["a@b.cm", "a"], expected: ["a", "Make sure you've entered the right email address and password. Accounts are locked after 5 failed attempts."] }
+    { args: ["a@b.cm", "a"], expected: ["a", "Your password should be at least 10 characters"] }
   ];
 
   for (const test of testCases) {
     it(`sign in fails ${test.args.length} args`, async function() {
       await buyerLogin(test.args[0], test.args[1]);
+      await util.clickInputButton("Sign in");
       await util.matchText(test.expected[0], test.expected[1]);
     });
   }
@@ -17,12 +18,15 @@ describe("should fail sign in", function() {
 describe("should sign in", function() {
   it("buyer should be able to login", async function() {
     await buyerLogin(process.env.BUYER_EMAIL, process.env.BUYER_PASSWORD);
+    await util.clickButton("Menu");
+    await util.clickLink("Dashboard");
     await util.matchText("h1", "Dashboard");
     await signOut("buyer");
   });
 
   it("seller should be able to login", async function() {
     await sellerLogin(process.env.SELLER_EMAIL, process.env.SELLER_PASSWORD);
+    await util.clickLink("Dashboard");
     await util.matchText("h1", "Dashboard");
     await signOut("seller");
   });
